4a3b3de7b9a64601143af24bffc60b334d9d2d41
huna <seunghun.chan@gmail.com>
2018-02-08 16:54:20 +0900

    [chapter05] sending stream data using buffer

--- ch05/test06.js
+++ ch05/test06.js
@@ -12,7 +12,30 @@ server.on('request', (req, res) => {
   console.log('requested from client');
 
   const filename = path.join(__dirname, 'up.gif');
-  fs.createReadStream(filename, {flags: 'r'}).pipe(res);
+  const infile = fs.createReadStream(filename, {flags: 'r'});
+  let filelength = 0;
+  let curlength = 0;
+
+  fs.stat(filename, (err, stat) => {
+    if (err) throw err;
+    filelength = stat.size;
+  });
+
+  res.writeHead(200, {'Content-Type': 'image/gif'});
+
+  infile.on('readable', () => {
+    let chunk;
+    while (null !== (chunk = infile.read())) {
+      console.log(`read size: ${chunk.length} byte`);
+      curlength += chunk.length;
+      res.write(chunk, 'utf8', err => {
+        console.log(`Writing complete! ${curlength}/${filelength}`);
+        if (curlength >= filelength) {
+          res.end();
+        }
+      });
+    }
+  })
 });
 
 server.on('close', () => {
