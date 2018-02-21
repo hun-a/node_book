88709bc7cae319953da80c3be4ff9a2789dd8d5d
huna <seunghun.chan@gmail.com>
2018-02-21 11:15:25 +0900

    [chapter05] my first router: post

---
+++ ExpressExample/app8.js
@@ -0,0 +1,27 @@
+const express = require('express')
+    , http = require('http')
+    , path = require('path')
+    , bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.post('/process/login', (req, res, next) => {
+  const paramId = req.body.id || req.query.id;
+  const paramPassword = req.body.password || req.query.password;
+
+  res.writeHead('200', {'Content-Type':'text/html;charset=utf-8'});
+  res.write('<h1>Response from Express server</h1>');
+  res.write(`<div><p>Param ID: ${paramId}</p></div>`);
+  res.write(`<div><p>Param password: ${paramPassword}</p></div>`);
+  res.end();
+});
+
+http.createServer(app).listen(app.get('port'), () => {
+  console.log(`Express server is running ${app.get('port')}`);
+});
